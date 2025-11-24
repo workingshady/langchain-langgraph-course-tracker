3f04870821a1921a0033acf4a2a1b1c00eeec8a4
Shady Mansour <workingshady@GMAIL.COM>
2025-11-25 01:19:34 +0200

    feat(hello-world): update main script and add LangSmith report screenshots

--- sections/02_langchain_fundamentals/projects/hello-world/main.py
+++ sections/02_langchain_fundamentals/projects/hello-world/main.py
@@ -1,13 +1,58 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-
 def main():
-    print("Hello from lang-chain!")
-    print(f"GoogleApi : {os.environ.get("GOOGLE_API_KEY")}")
+    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+    if not GOOGLE_API_KEY:
+        raise EnvironmentError("GOOGLE_API_KEY not set in environment or .env file")
+
+    information = """
+    Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
+
+Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; his Canadian citizenship is congenital, his mother having been born there. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals.
+
+Musk was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated as president in early 2025, Musk served as Senior Advisor to the President and as the de facto head of DOGE. After a public feud with Trump, Musk left the Trump administration and returned to managing his companies.
+
+Musk is a supporter of global far-right figures, causes, and political parties. His political activities, views, and statements have made him a polarizing figure. Musk has been criticized for COVID-19 misinformation, promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted considerable public backlash, particularly in response to DOGE.
+    """
+
+    summary_template = """
+Given the following information about a person:
+
+{information}
+
+Please provide:
+1. A short summary of the individual.
+2. Two interesting facts about them.
+
+Your response should be clear, informative, and suitable for readers looking for both a quick overview and intriguing details about the person.
+    """
+
+    # Construct a prompt template for the summary
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template
+    )
+
+    # Set up the Google Generative AI LLM
+    genai_llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        api_key=GOOGLE_API_KEY,
+        temperature=0.2,
+    )
+
+    # Compose the chain: prompt -> LLM
+    chain = summary_prompt_template | genai_llm
+    response = chain.invoke({"information": information})
+    print(response.content)
+
 
 
 if __name__ == "__main__":
