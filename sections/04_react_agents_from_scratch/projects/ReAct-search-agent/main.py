d86c8d99d4c3e663855a4094507cb5234c4abd9e
Shady Mansour <workingshady@GMAIL.COM>
2025-11-27 13:21:37 +0200

    feat(react-agent): update main and add prompt & schemas

--- sections/04_react_agents_from_scratch/projects/ReAct-search-agent/main.py
+++ sections/04_react_agents_from_scratch/projects/ReAct-search-agent/main.py
@@ -2,12 +2,15 @@ import os
 from tabnanny import verbose
 
 from dotenv import load_dotenv
+from langchain_core import output_parsers
 
 load_dotenv()
 
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
 #from langchain import hub  --> hub module was removed
 # from langchain.agents import AgentExecutor
@@ -15,6 +18,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 from langsmith import Client
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
@@ -41,13 +47,29 @@ smith_client = Client()
 prompt_id = "hwchase17/react"
 react_prompt = smith_client.pull_prompt("hwchase17/react", include_model=True)
 
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=[
+        "tool_names",
+        "input",
+        "agent_scratchpad"
+    ],
+    partial_variables={"format_instructions": output_parser.get_format_instructions()}
+)
+
+
 agent = create_react_agent(
     llm=genai_llm,
     tools=tools,
     prompt=react_prompt
 )
 agent_executor = AgentExecutor(agent,tools=tools,verbose=True)
-chain = agent_executor
+# chain = agent_executor
+
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
@@ -57,6 +79,9 @@ def main():
         }
     )
     print(result)
+    #result = agent.run(query)
+    #parsed = output_parser.parse(result["output"])
+
 
 if __name__ == "__main__":
     main()
